27547fc3ea76f8d10f71bbc4f47ea23c0f72c916
ShihabSarar12 <shihabsararchamok@gmail.com>
2024-10-26 20:00:23 +0600

    added s3 feature

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,29 +1,25 @@
 import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import multerS3 from 'multer-s3';
+import AWS from 'aws-sdk';
 import expressAsyncHandler from 'express-async-handler';
 import { pool } from '../config/db.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const uploadDir = path.join(__dirname, '..', 'pictures');
-
-//TODO have to fix this before production
-// if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, uploadDir);
-	},
-	filename: (req, file, cb) => {
-		const uniqueSuffix = `${Date.now()}-${file.originalname}`;
-		cb(null, uniqueSuffix);
-	},
+const s3 = new AWS.S3({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
 });
 
 const upload = multer({
-	storage,
+	storage: multerS3({
+		s3: s3,
+		bucket: process.env.AWS_S3_BUCKET_NAME,
+		acl: 'public-read',
+		key: (req, file, cb) => {
+			const uniqueSuffix = `${Date.now()}-${file.originalname}`;
+			cb(null, uniqueSuffix);
+		},
+	}),
 	limits: { fileSize: 1024 * 1024 * 5 },
 	fileFilter: (req, file, cb) => {
 		if (file.mimetype.startsWith('image/')) cb(null, true);
@@ -31,16 +27,13 @@ const upload = multer({
 	},
 });
 
-//TODO: implement cleanup middleware
 const uploadFile = expressAsyncHandler(async (req, res, next) => {
 	upload.single('img')(req, res, function (err) {
 		if (err instanceof multer.MulterError)
 			throw new Error(`message: ${err.message}`);
 		else if (err) throw new Error(`message: ${err.message}`);
-		const fileRoute = path
-			.join('pictures', req.file.filename)
-			.replace(/\\/g, '/');
-		req.filePath = `${req.protocol}://${req.get('host')}/${fileRoute}`;
+
+		req.filePath = req.file.location;
 		next();
 	});
 });
@@ -52,14 +45,8 @@ const uploadMultiple = expressAsyncHandler(async (req, res, next) => {
 		} else if (err) {
 			throw new Error(`message: ${err.message}`);
 		}
-		const fileRoutes = req.files.map((file) => {
-			const fileRoute = path
-				.join('pictures', file.filename)
-				.replace(/\\/g, '/');
-			return `${req.protocol}://${req.get('host')}/${fileRoute}`;
-		});
 
-		req.filePaths = fileRoutes;
+		req.filePaths = req.files.map((file) => file.location);
 		next();
 	});
 });
@@ -80,23 +67,21 @@ const deleteFile = expressAsyncHandler(async (req, res, next) => {
 			message: 'There is no facility by this facility_id',
 		});
 	}
-	const fileName = path.basename(img);
-	const fullFilePath = path.join(uploadDir, fileName);
 
-	fs.unlink(fullFilePath, (err) => {
-		if (err) {
-			if (err.code === 'ENOENT') {
-				res.status(400).json({
-					message: 'File not found',
+	s3.deleteObject(
+		{
+			Bucket: process.env.AWS_S3_BUCKET_NAME,
+			Key: img.split('/').pop(),
+		},
+		(err) => {
+			if (err) {
+				return res.status(500).json({
+					message: 'Error deleting file from S3',
 				});
-				return;
-			} else {
-				throw new Error('Error deleting file');
 			}
-		} else {
 			next();
 		}
-	});
+	);
 });
 
-export { uploadFile, uploadDir, uploadMultiple, deleteFile };
+export { uploadFile, uploadMultiple, deleteFile };
